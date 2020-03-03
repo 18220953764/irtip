ad755841742595d61ef0ec5cf2caf77b4946646b
38930 <18220953764@163.com>
2020-03-04 01:31:45 +0800

    1、新增itrip-commons项目 2、新增itrip-provider-commons项目

---
+++ itrip-commons/src/main/java/com/whackon/itrip/App.java
@@ -0,0 +1,13 @@
+package com.whackon.itrip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ itrip-eureka-server/src/main/java/com/whackon/itrip/EurekaServerStarter.java
@@ -0,0 +1,16 @@
+package com.whackon.itrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 爱旅行注册中心启动类
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerStarter.class,args);
+	}
+}
